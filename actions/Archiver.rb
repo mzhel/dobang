38735d038f4514e3286bd01c6f7f10cdf4e26c48
mzhel <bitcatcher@fastmail.fm>
2013-11-30 13:56:27 +0600

    Archiver module added.

---
+++ actions/Archiver.rb
@@ -0,0 +1,88 @@
+class Archiver
+
+	def initialize
+		
+		@pathAliases = {}
+		
+		ParsePathAliases(ENV['HOME'] + '/gccbuildconf')
+	
+	end
+	
+	def TouchDir(name)
+	
+		begin
+		
+			Dir.mkdir(name)
+		
+		rescue
+		
+		end
+		
+	end
+	
+
+	def ParsePathAliases(path)
+
+		if File.exist? path
+		
+			File.open(path) do |f|
+		
+				f.each_line do |l|
+			
+					name, value = l.split('=')
+				
+					@pathAliases[name] = value.chop!
+				
+				end
+		
+			end
+
+		end
+	
+	end
+
+	def Objs(objLst)
+	
+		r = ""
+		
+		objLst.each do |o|
+		
+			r << "#{SubVar(o)} "
+		
+		end
+		
+		r		
+	
+	end
+
+	def ArchiverString(opts, objLst)
+	
+		r = "ar -cvq "
+		
+		TouchDir(opts['TARGETDIR'])
+		
+		r << %Q{"#{opts['TARGETDIR']}#{opts['TARGETNAME']}" }
+		
+		r << Objs(objLst)
+		
+		r
+		
+	end
+	
+	#
+	# Callbacks
+	#
+	
+	def Do(opts)
+	
+		str = ArchiverString(opts, GetVar(:objects))
+	
+		puts str
+	
+		out %x[#{str}]
+		
+		($?.exitstatus > 0)?(false):(true)
+		
+	end
+
+end
