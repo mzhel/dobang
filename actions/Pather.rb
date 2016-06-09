f63c053fed65051fb106434e1c4d5d1b6e7ec586
gmnx <thegmnx@gmail.com>
2016-06-09 15:37:22 +0600

    Added alias support to Pather module.

--- actions/Pather.rb
+++ actions/Pather.rb
@@ -4,9 +4,39 @@ class Pather
 	# Callbacks from core
 	#
 	
+	def initialize
+	
+		@pathAliases = {}
+		
+		ParsePathAliases(ENV['home'] + '/pather_aliases')
+	
+	end
+	
+	def ParsePathAliases(path)
+
+		if File.exist? path
+		
+			File.open(path) do |f|
+		
+				f.each_line do |l|
+			
+					name, value = l.split('=')
+				
+					@pathAliases[name] = value.chop!
+				
+				end
+		
+			end
+
+		end
+	
+	end
+	
 	def Do(opts)
 
 		opts.each_pair do |key, value|
+		
+			value = @pathAliases[value] if @pathAliases[value]
 		
 			out "Adding %s to PATH environment variable"%value
 		
